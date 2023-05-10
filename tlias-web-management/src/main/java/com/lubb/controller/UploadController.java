2729fea898fb3df830ca7755ac7fa2be89781f43
lubb <2820843645@qq.com>
2023-05-10 21:58:40 +0800

    alter

---
+++ tlias-web-management/src/main/java/com/lubb/controller/UploadController.java
@@ -0,0 +1,32 @@
+package com.lubb.controller;
+
+import com.lubb.pojo.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+@RestController
+public class UploadController {
+
+    @PostMapping("upload")
+    public Result upload(String username, Integer age, MultipartFile image) throws IOException {
+        log.info("文件上传");
+        //springboot 中默认单个文件最大为1M
+
+        String originalFilename = image.getOriginalFilename();
+        int index = originalFilename.lastIndexOf(".");
+        String extname = originalFilename.substring(index);
+        String newFIleName = UUID.randomUUID().toString() + extname;
+        log.info("新文件名{}", newFIleName);
+
+        //
+        image.transferTo(new File("E:\\" + newFIleName));
+        return Result.success();
+    }
+}
