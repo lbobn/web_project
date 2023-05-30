9b3bd93d6335bc1be2e2098a07fbba6e7638e457
lubb <2820843645@qq.com>
2023-05-30 14:33:34 +0800

    alter

--- tlias-web-management/src/main/java/com/lubb/TliasWebManagementApplication.java
+++ tlias-web-management/src/main/java/com/lubb/TliasWebManagementApplication.java
@@ -2,7 +2,11 @@ package com.lubb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+import javax.servlet.annotation.WebServlet;
+
+@ServletComponentScan//开启对servlet或javaweb组件的支持
 @SpringBootApplication
 public class TliasWebManagementApplication {
 
--- tlias-web-management/src/main/java/com/lubb/controller/LoginController.java
+++ tlias-web-management/src/main/java/com/lubb/controller/LoginController.java
@@ -4,11 +4,15 @@ package com.lubb.controller;
 import com.lubb.pojo.Emp;
 import com.lubb.pojo.Result;
 import com.lubb.service.EmpService;
+import com.lubb.utils.JwtUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -20,6 +24,17 @@ public class LoginController {
     public Result login(@RequestBody Emp emp) {
         log.info("用户登录{}", emp);
         Emp e = empService.login(emp);
-        return e != null ? Result.success() : Result.error("用户名或密码错误！");
+
+        if (e != null) {
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("id", e.getId());
+            claims.put("name", e.getName());
+            claims.put("username", e.getUsername());
+
+            String jwt = JwtUtils.generateJwt(claims);
+            return Result.success(jwt);
+        }
+
+        return Result.error("用户名或密码错误！");
     }
 }
